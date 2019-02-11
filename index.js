dc3200fdda9354dad023817b7abca08e65a8e672
Pierson Brannan <plbrannan95@gmail.com>
2019-02-11 18:18:05 -0500

    Add route to handle :id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const parser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("./db/connection");
-const Post = require("./models/Post")
+const Post = require("./models/Post");
 
 const app = express();
 
@@ -11,16 +11,20 @@ app.use(parser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    Post.find()
-        .then(results => {
-            res.json(results);
-        })
-        // .catch(err => {
-        //     console.log(err);
-        // });
-}); 
+    Post.find().then(results => {
+        res.json(results);
+    });
+    // .catch(err => {
+    //     console.log(err);
+    // });
+});
+
+app.get("/show/:id", (req, res) => {
+    Post.findById(req.params.id).then(result => {
+        res.json(result);
+    });
+});
 
 app.listen(app.get("port"), () => {
     console.log("Coming in LIVE from port " + app.get("port") + "!");
 });
-
